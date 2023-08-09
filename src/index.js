590634e697697ef5a3d2083296a412ccbcfc6e0b
NicolasKuban <nicrad@yandex.ru>
2023-08-09 19:25:17 +0300

    fix: Удалены строки вывода сообщений в консоль, добавлены комментарии

--- src/index.js
+++ src/index.js
@@ -1,74 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  let symbolOpen = {}
-  let symbolClose = []
+  let symbolOpen = {}   // Открыающие символы
+  let symbolClose = []  // Закрывающие символы
+  let stack = []        // Стэк обработки строки
 
-  console.log(`"${str}", [${bracketsConfig}]`)
+  // Заполнить справочники открывающих и закрывающих символов
   for (i = 0; i < bracketsConfig.length; i++) {
     symbolOpen[bracketsConfig[i][0]] = bracketsConfig[i][1]
     symbolClose.push(bracketsConfig[i][1])
-    // console.log([bracketsConfig[i][1]])
   }
 
-  console.log(symbolOpen)
-  console.log(symbolClose)
-
-  let stack = []
+  // Обработка строки (добавление в стэк, извлечение из стэка)
   for (i = 0; i < str.length; i++) {
+    // Если символ открывающий
     if (symbolOpen[str[i]]) {
+      // Является ли этот символ и закрывающим и такой же символ лежит на вершине стэка
       if (symbolOpen[str[i]] === str[i] && stack[stack.length - 1] === str[i]) {
-        console.log("??", symbolOpen[str[i]] === str[i])
-        console.log("--1", stack[stack.length - 1], str[i], stack[stack.length - 1] === str[i])
+        // удаляем символ из стэка
         stack.pop()
       } else {
+        // добавляем символ в стэк
         stack.push(str[i])
-        console.log("++", stack[stack.length - 1], str[i])
       }
     } else {
-
+      // является ли символ закрывающим
       if (symbolClose.includes(str[i])) {
+        // Соответствует ли закрывающий символ открывающему
         if (symbolOpen[stack[stack.length - 1]] === str[i]) {
-          console.log("--2", stack[stack.length - 1], str[i], stack[stack.length - 1] === str[i])
+          // удаляем открывающий символ с вершины стэка
           stack.pop()
         } else {
-          console.log("break");
+          // Последовательность символов не соблюдена, выход из функции
           return false
         }
-      // if (symbolOpen[str[i]]) {stack.push(str[i])}
       }
     }
-    console.log("==", i, stack)
-
-  //   if (str[i] === bracketsConfig[0][0]) {
-  //     symbol[bracketsConfig[0][0]] += 1
-  //     console.log(symbol)
-
-  //   } else if (str[i] === bracketsConfig[0][1]){
-  //     symbol[bracketsConfig[0][0]] -= 1
-  //     console.log(symbol)
-  //   }
-  //   if (symbol[bracketsConfig[0][0]] < 0) {return false}
-
   }
-
-  // for (i = 0; i < bracketsConfig.length; i++) {
-  //   if (symbol[bracketsConfig[i][0]] !== 0) {return false}
-  // }
-
-  
+  // При верной последовательности стэк должен быть пуст
   return (stack.length) ? false : true
 }
-
-
-const config1 = [['(', ')']];
-const config2 = [['(', ')'], ['[', ']']];
-const config3 = [['(', ')'], ['[', ']'], ['{', '}']];
-const config4 = [['|', '|']];
-const config5 = [['(', ')'], ['|', '|']];
-const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
-const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
-
-
-
-fun = module.exports
-console.log(fun('111115611111111222288888822225577877778775555666677777777776622222', config6))

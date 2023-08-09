14751007408516e76a508ca624d6645ba37bf29e
NicolasKuban <nicrad@yandex.ru>
2023-08-09 10:49:15 +0300

    feat: создана функция для разбора круглых скобок

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let symbol = {}
+  for (i = 0; i < bracketsConfig.length; i++) {
+    symbol[bracketsConfig[i][0]] = 0
+  }
+
+  for (i = 0; i < str.length; i++) {
+    if (str[i] === bracketsConfig[0][0]) {
+      symbol[bracketsConfig[0][0]] += 1
+      console.log(symbol)
+
+    } else if (str[i] === bracketsConfig[0][1]){
+      symbol[bracketsConfig[0][0]] -= 1
+      console.log(symbol)
+    }
+    if (symbol[bracketsConfig[0][0]] < 0) {return false}
+  }
+
+  for (i = 0; i < bracketsConfig.length; i++) {
+    if (symbol[bracketsConfig[i][0]] !== 0) {return false}
+  }
+
+  return true
 }
+
+fun = module.exports
+console.log(fun('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]))
